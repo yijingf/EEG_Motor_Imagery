17d792b1791f82af5bcdaf0c17dcb2f10bfb4b3c
Xinhui Li <sucyli1017@gmail.com>
2021-01-10 21:17:49 -0500

    update training_curve.py and mne version in requirements.txt

--- src/training_curve.py
+++ src/training_curve.py
@@ -1,7 +1,18 @@
+""" 
+Plot the training curve of accuracy and loss
+
+Usage: python training_curve.py <model name>
+<model name> is a folder name under ../result, which is usuall a time stamp.
+For example, run 'python training_curve.py 20210110' to plot 
+'../result/20210110/train_log.txt' and '../result/20210110/val_log.txt'
+
+Authors: Yijing Feng, Xinhui Li
+"""
+
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-
 from scipy.interpolate import make_interp_spline, BSpline
 
 root_dir = '../result/'
@@ -23,18 +34,19 @@ def load_data(model, mode):
     
     return step, loss, acc
 
-def plot_data(steps, metrics, metric_type, filename, smooth_factor=5):
+def plot_data(steps, metrics, metric_type, filename, smooth_factor=1600):
     # Draw lines
     fig = plt.figure()
     for i, mode in enumerate(['Training', 'Validation']):
-        step_new = np.linspace(min(steps[i]), max(steps[i]), int(len(steps[i])/smooth_factor))
+        step_new = np.linspace(min(steps[i]), max(steps[i]), int(max(steps[i])/smooth_factor))
         spl = make_interp_spline(steps[i], metrics[i], k=3) #BSpline object
         metric_smooth = spl(step_new)
+        step_new = step_new / smooth_factor
         plt.plot(step_new, metric_smooth, label="{} {}".format(mode, metric_type))
-    
+
     # Create plot
     plt.title("Learning Curve - {}".format(metric_type))
-    plt.xlabel("Steps"), plt.ylabel(metric_type), plt.legend(loc="best")
+    plt.xlabel("Epochs"), plt.ylabel(metric_type), plt.legend(loc="best")
     plt.tight_layout()
     
     fig.savefig(filename)
@@ -43,10 +55,13 @@ def plot_data(steps, metrics, metric_type, filename, smooth_factor=5):
 def main(model):
     train_step, train_loss, train_acc = load_data(model, 'train')
     val_step, val_loss, val_acc = load_data(model, 'val')
-
+    
     filename = os.path.join(root_dir, model, 'LossCurve')
     plot_data([train_step, val_step], [train_loss, val_loss], 'Loss', filename)
     
     filename = os.path.join(root_dir, model, 'AccCurve')
     plot_data([train_step, val_step], [train_acc, val_acc], 'Accuracy', filename)
     return
+
+if __name__ == "__main__":
+    main(sys.argv[1])
