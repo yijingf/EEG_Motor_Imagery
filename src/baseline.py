a3c0ab0cb513a83ce432e16ca157157c7c75a549
xinyuan <xz2906@columbia.edu>
2021-01-12 10:50:31 -0500

    Update baseline.py and eeg_BCI2000_ml.py

--- src/baseline.py
+++ src/baseline.py
@@ -1,53 +1,73 @@
-# import numpy as np
-# from sklearn import svm
-# from sklearn.metrics import accuracy_score
-# from eeg_loader import *
-# from config import SUBs, freq_bands
-# from bandpower import *
-
-# # Preprocess config
-# l_freq = 4
-# h_freq = 30
-# resample_freq = 100 # original sfreq is 160
-
-# # load data
-# data_loader = DataLoader(window_len=100, overlap=0) # without overlapping, window_len=100 unit?
-# # take a window sufficiently long to encompasses at least two full cycles of the lowest frequency of interest. 
-# # https://raphaelvallat.com/bandpower.html
-# # 4hz -> 0.25s -> 2 cycles 0.5s -> resample_freq=100: 50 samples? 
-
-# X, y, _ = data_loader.load_data(SUBs, 
-#                                 l_freq=l_freq, h_freq=h_freq, 
-#                                 resample_sfreq=resample_freq, mesh=False)
-# X = np.array([getBandPower_Pool(x, freq_bands, resample_freq) for x in X])
-
-# # X is a matrix with the shape of (n, 3, 64) where n is the number of samples?, 3 is the number of frequency bands and 64 is the number of channels
-
-# # to do: train/valid/test split 60 20 20 
-
-
-
-
+# This code defines functions for three ML methods in order to assess baseline performace
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+
+# to use commonly default settings, call SVM(), RandForest(), LogReg()
+def SVM(train_data, train_labels, test_data, test_labels, verbose = 1):
+    clf = SVC(C = 1, gamma = 'scale')
+    clf.fit(train_data, train_labels)
+    pred_labels = clf.predict(test_data)
+    if verbose != -1:
+        print('\n Test Accuracy: %f' % accuracy_score(test_labels, pred_labels))
+    return(pred_labels, accuracy_score(test_labels, pred_labels))
+
+def RandForest(train_data, train_labels, test_data, test_labels, verbose = 1):
+    clf = RandomForestClassifier(n_estimators = 100)
+    clf.fit(train_data, train_labels)
+    pred_labels = clf.predict(test_data)
+    if verbose != -1:
+        print('\n Test Accuracy: %f' % accuracy_score(test_labels, pred_labels))
+    return(pred_labels, accuracy_score(test_labels, pred_labels))
 
 
-def SVM(data, labels, K, verbose=1):
-    '''
+# All Baselines using default parameters
+def CV_Baselines(data, labels, test_data, test_labels, K = 0, verbose = 1):
+    
+    clfs = [
+        SVC(C = 1, gamma = 'scale'),
+        LogisticRegression(penalty = 'l1', C = 1, solver='liblinear'),
+        RandomForestClassifier(n_estimators = 100)
+        ]
+    
+    if K > 0:
+        acc_dict = dict()
+        for clf in clfs:
+            acc_dict[str(clf.__class__.__name__)] = cross_val_score(clf, data, labels, cv = K).mean()
+            if verbose != -1:
+                print(f'classifier: ', clf.__class__.__name__, 'avg CV acc: ', acc_dict[str(clf.__class__.__name__)])
+    
+    if K == 0:
+        acc_dict = dict()
+        for clf in clfs:
+            clf.fit(data, labels)
+            pred_labels = clf.predict(test_data)
+            acc_dict[str(clf.__class__.__name__)] = accuracy_score(test_labels, pred_labels)
+            if verbose != -1:
+                print(f'classifier: ', clf.__class__.__name__, 'test acc: ', acc_dict[str(clf.__class__.__name__)])
+    
+    return(acc_dict)
+
+
+# to use hyperparameters chosen by grid search, call SVM_gridSearch(), LogReg_gridSearch(), RandForest_gridSearch()
+def SVM_gridSearch(data, labels, test_data, test_labels, K, verbose=1):
+    """    
     Support Vector Machine
-    
-    C and γ were selected by grid-search in the range 
-    of {0.01, 0.1, 1, 10, 100} and {0.1, 0.2, ..., 1, 2, ..., 10} respectively.
-    
-    '''
-    def CV_SVM(data, labels, K):
+    input: data and labels should contain train and validation sets
+    C is selected by grid-search in the range of {0.01, 0.1, 1, 10, 100}.
+    """
+    def SVM_CV(data, labels, K):
         #perform cross-validation and SVM classification
-        hyperparams = {'C' : [0.01, 0.1, 1, 10, 100],
-                       'gamma': np.arange(0.1, 10, 0.1) }
+        hyperparams = {'C': [0.01, 0.1, 1, 10, 100],
+                       'gamma': np.arange(0.1, 10, 0.1)}
         cross_valid = StratifiedKFold(n_splits = K, 
                                       shuffle=True, random_state = 0)
         grid_search = GridSearchCV(SVC(), 
@@ -56,84 +76,69 @@ def SVM(data, labels, K, verbose=1):
         grid_search.fit(data, labels)
         return grid_search.best_params_, grid_search.best_score_
     
-    PARAM, score = CV_SVM(data=data, labels=labels, K=K)
+    PARAM, score = SVM_CV(data=data, labels=labels, K = K)
+    
+    if verbose != -1:
+        print('f best parameter: ', PARAM)
+        print(f'best CV acc', score) 
     
     clf = SVC(C = PARAM['C'], gamma = PARAM['gamma'])
     clf.fit(data, labels)
-    pred_labels = clf.predict(data) 
+    pred_labels = clf.predict(test_data)
     
     if verbose != -1:
-        print('Done.\nAccuracy: %f' % accuracy_score(labels, pred_labels))
+        print('test acc: %f' % accuracy_score(test_labels, pred_labels))
     
-    return(pred_labels, accuracy_score(labels, pred_labels))
+    return(pred_labels, accuracy_score(test_labels, pred_labels))
 
 
-
-
-def LogReg(data, labels, K, verbose=1):
-    '''
-    Logistic Regression: l1-regularization was used to introduce sparsity.
-    Regularization parameter C was chosen in the range of [10e−2, 10e3].
-    '''
-    def CV_LogReg(data, labels, K):
-        # perform cross-validation for Logistic Regression classification
-        hyperparams = {'C' : [0.01, 0.1, 1, 10, 100, 1000]}
-        cross_valid = StratifiedKFold(n_splits = K, 
-                                      shuffle = True, random_state = 0)
-        grid_search = GridSearchCV(LogisticRegression(penalty = 'l1',
-                                                      solver='liblinear'), 
-                                   param_grid = hyperparams, 
-                                   cv = cross_valid)
-        grid_search.fit(data, labels)
-        return grid_search.best_params_, grid_search.best_score_
-    
-    PARAM, score = CV_LogReg(data=data, labels=labels, K=K)
-    
-    clf = LogisticRegression(penalty = 'l1',
-                             C = PARAM['C'], 
-                             solver='liblinear')
-    clf.fit(data, labels)
-    pred_labels = clf.predict(data) 
-    
-    if verbose != -1:
-        print('Done.\nAccuracy: %f' % accuracy_score(labels, pred_labels))
-    
-    return(pred_labels, accuracy_score(labels, pred_labels))
-
-
-
-
-def RandForest(data, labels, K, DEPTH, verbose=1):
-    '''
-    random forest 
-    The number of RandForest estimators is chosen within the set of {5, 10, 20, 50, 100, 500, 1000}.
-    '''
-    def CV_RandForest(data, labels, K):
+def RandForest_gridSearch(data, labels, test_data, test_labels, K, verbose=1):
+    """
+    Random Forest 
+    The number of estimators is chosen in {5, 10, 20, 50, 100, 500, 1000}.
+    """
+    def RandForest_CV(data, labels, K):
         # perform cross-validation for RF classification
-        hyperparams = {'n_estimators' : [5, 10, 20, 50, 100, 500, 1000]}
+        hyperparams = {'n_estimators' : [5, 10, 20, 50, 100, 500, 1000],
+                       'min_samples_split': [2, 5, 10]}
         cross_valid = StratifiedKFold(n_splits = K, 
                                       shuffle = True, random_state = 0)
-        grid_search = GridSearchCV(RandomForestClassifier(max_depth = DEPTH), 
+        grid_search = GridSearchCV(RandomForestClassifier(), 
                                    param_grid = hyperparams, 
                                    cv = cross_valid)
         grid_search.fit(data, labels)
         return grid_search.best_params_, grid_search.best_score_
     
-    PARAM, score = CV_RandForest(data=data, labels=labels, K=K)
+    PARAM, score = RandForest_CV(data=data, labels=labels, K=K)
+    
+    if verbose != -1:
+        print('f best parameter: ', PARAM)
+        print(f'best CV acc', score) 
     
     clf = RandomForestClassifier(n_estimators = PARAM['n_estimators'], 
-                                 max_depth = DEPTH)
+                                 min_samples_split = PARAM['min_samples_split'])
     clf.fit(data, labels)
-    pred_labels = clf.predict(data) 
+    pred_labels = clf.predict(test_data)
     
     if verbose != -1:
-        print('Done.\nAccuracy: %f' % accuracy_score(labels, pred_labels))
+        print('test acc: %f' % accuracy_score(test_labels, pred_labels))
     
-    return(pred_labels, accuracy_score(labels, pred_labels))
+    return(pred_labels, accuracy_score(test_labels, pred_labels))
 
 
 
+# Generate data
+from sklearn.datasets import make_blobs
+cluster_std = 5
+num_features_for_samples = 4
+num_samples_total = 1000
+test_ratio = 0.20
+X, y = make_blobs(n_samples = num_samples_total, 
+                  n_features = num_features_for_samples, 
+                  cluster_std = cluster_std)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_ratio)
 
-pred_y, acc_score = SVM(data = np.reshape(X, (-1,3*64)), labels = y, K=3)
-# pred_y, acc_score = LogReg(data = np.reshape(X, (-1,3*64)), labels = y, K=3)
-# pred_y, acc_score = RandForest(data = np.reshape(X, (-1,3*64)), labels = y, K=3, DEPTH = 2)
+# test function
+acc_dict = CV_Baselines(X_train, y_train, X_test, y_test, K = 3)
+RandForest_gridSearch(data = X_train, labels = y_train, 
+                      test_data = X_test, test_labels = y_test, K = 3)
--- src/eeg_BCI2000_ml.py
+++ src/eeg_BCI2000_ml.py
@@ -3,28 +3,33 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from eeg_loader import *
 from config import SUBs, freq_bands
+from bandpower import *
+from baseline_ml import *
+from sklearn.model_selection import train_test_split
 
 # Preprocess config
 l_freq = 4
 h_freq = 30
 resample_freq = 100 # original sfreq is 160
 
-
 # load data
-data_loader = DataLoader(window_len=100, overlap=0)
-X, Y, _ = data_loader.load_data(SUBs, l_freq=l_freq, h_freq=h_freq, resample_sfreq=resample_freq, mesh=False)
+data_loader = DataLoader(window_len=100, overlap=0) # machine learning window_len 100, deep learning window_len: 20-40
+X, y, _ = data_loader.load_data(SUBs, 
+                                l_freq=l_freq, h_freq=h_freq, 
+                                resample_sfreq=resample_freq, mesh=False)
 X = np.array([getBandPower_Pool(x, freq_bands, resample_freq) for x in X])
-
 # X is a matrix with the shape of (n, 3, 64) where n is the number of samples, 3 is the number of frequency bands and 64 is the number of channels
 
-# Todo: Split train/test/validation set
+X = X.reshape((-1, 3*64))
+# train_ratio = 0.60
+# validation_ratio = 0.20
+test_ratio = 0.20
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_ratio)
+# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = validation_ratio/(1-test_ratio))
 
+# default setting 
+acc_dict = CV_Baselines(X_train, y_train, X_test, y_test, K = 5)
 
-# train svm model
-clf = svm.SVC(gamma='scale')
-clf.fit(train_X, train_y)  
-
-# predict validation set 
-valid_pre_y = clf.predict(valid_X) 
-val_acc = accuracy_score(valid_pre_y, valid_y)
-print('valid accuracy: ', val_acc)
+# grid searched parameters
+pred_y_rf, acc_rf = RandForest_gridSearch(data = X_train, labels = y_train, test_data = X_test, test_labels = y_test, K = 3)
+pred_y_svm, acc_svm = SVM_gridSearch(data = X_train, labels = y_train, test_data = X_test, test_labels = y_test, K = 3)
